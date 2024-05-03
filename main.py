d249703635c8902dd2f6740b876ff43a4343ea0d
luis96920 <lucasm3938@gmail.com>
2024-05-03 17:52:55 -0400

    added settings in this chainlit app

--- main.py
+++ main.py
@@ -70,27 +70,36 @@ async def chat_profile():
             markdown_description="The underlying large language model model is **deepseek-coder 1B** for coding assistance with quantization 4-bit.",
             # icon="https://picsum.photos/200",
         ),
+        cl.ChatProfile(
+            name="Qwen on local",
+            markdown_description="The underlying large language model model is **qwen 4B** wtih quantization 4-bit.",
+            # icon="https://picsum.photos/200",
+        ),
     ]
 
 @cl.on_chat_start
 async def on_chatstart():
-    
-    # # Sending an image with the local file path
-    # elements = [
-    # cl.Image(name="image1", display="inline", path="groq.jpeg")
-    # ]
-    # await cl.Message(content="Hello there, I am Groq. How can I help you ?", elements=elements).send()
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Role",
+                label="Agent Role",
+                values=["General Knowledge Expert", "Generative AI Expert", "DevOps Expert", "Mathematician"],
+                initial_index=0,
+            ), 
+            Slider(
+                id="Temperature",
+                label="LLM Temperature",
+                initial=0.3,
+                min=0,
+                max=1,
+                step=0.1,
+            ),
+        ]
+    ).send()
 
     model_selected = cl.user_session.get("chat_profile")
     
-    gcp_project_ID = "aihub123-test1"
-
-    if 'gpt' in model_selected.lower():
-        vendor_name = "OpenAI"
-        apikey_html =  "https://platform.openai.com/api-keys"
-    else:
-        vendor_name = "Groq"
-        apikey_html = "https://console.groq.com/keys"
 
     await cl.Message(
         content=f"You are chatting with LLM **{model_selected.split()[0]}** via {model_selected.split()[2]}."
@@ -101,7 +110,7 @@ async def on_chatstart():
         [
             (
                 "system",
-                "You're a very knowledgeable Machine Learning Engineer.",
+                f"You are a very knowledgeable {settings['Role']}.",
             ),
             ("human", "{question}"),
         ]
@@ -111,35 +120,38 @@ async def on_chatstart():
     if 'local' in model_selected.lower():
         if 'llama3' in model_selected.lower():
             # 4.7GB (https://ollama.com/library)
-            model = Ollama(model="llama3")
+            model = Ollama(model="llama3", temperature=settings["Temperature"])
         elif 'gemma' in model_selected.lower():
             # 5.0GB
             model = Ollama(model="gemma")
         elif 'codegemma' in model_selected.lower():
             # 5.0GB
-            model = Ollama(model="codegemma")
+            model = Ollama(model="codegemma", temperature=settings["Temperature"])
         elif 'phi' in model_selected.lower():
             # 2.3GB
-            model = Ollama(model="phi3")
+            model = Ollama(model="phi3", temperature=settings["Temperature"])
         elif 'deepseek-coder' in model_selected.lower():
             # ~800MB
-            model = Ollama(model="deepseek-coder")
+            model = Ollama(model="deepseek-coder", temperature=settings["Temperature"])
+        elif 'qwen' in model_selected.lower():
+            # ~2.3GB
+            model = Ollama(model="qwen", temperature=settings["Temperature"])
     elif 'gpt' not in model_selected.lower() and 'groq' in model_selected.lower():
         if 'gemma' in model_selected.lower():
-            model = ChatGroq(temperature=0, model_name='gemma-7b-it')
+            model = ChatGroq(temperature=settings["Temperature"], model_name='gemma-7b-it')
         elif 'mixtral' in model_selected.lower():
-            model = ChatGroq(temperature=0, model_name='mixtral-8x7b-32768')
+            model = ChatGroq(temperature=settings["Temperature"], model_name='mixtral-8x7b-32768')
         elif 'llama3-8b' in model_selected.lower():
-            model = ChatGroq(temperature=0, model_name='llama3-8b-8192')
+            model = ChatGroq(temperature=settings["Temperature"], model_name='llama3-8b-8192')
         elif 'llama3-70b' in model_selected.lower():
-            model = ChatGroq(temperature=0, model_name='llama3-70b-8192')
+            model = ChatGroq(temperature=settings["Temperature"], model_name='llama3-70b-8192')
         else:
             print("model not found")
     elif 'gpt' in model_selected.lower() and '3.5' in model_selected.lower():
         # https://platform.openai.com/docs/models/overview
-        model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+        model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=settings["Temperature"])
     elif 'gpt' in model_selected.lower() and '-4' in model_selected.lower():
-        model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
+        model = ChatOpenAI(model="gpt-4-turbo", temperature=settings["Temperature"])
     
     runnable = prompt | model | StrOutputParser()
     cl.user_session.set("runnable", runnable)
